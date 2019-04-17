abb2fec1cad3c252f335724dfc96cd26d8b4b271
laurenkerns <laurenkerns@gmail.com>
2019-04-17 16:34:09 -0400

    some code clean up in app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
-// Grab the articles as a json
+//////////Grab the articles as a json///////////
 $.getJSON("/articles", data => {
     // For each one
     for (let i = 0; i < data.length; i++) {
@@ -10,75 +10,67 @@ $.getJSON("/articles", data => {
     }
   });
   
-  
-  // Whenever someone clicks a p tag
+  ///////// On CLick note function///////////////
   $(document).on("click", "#note", function() {
-    // Empty the notes from the note section
+    // Empty the notes 
     $("#notes").empty();
-    // Save the id from the p tag
+    // Save the id from the note id
     const thisId = $(this).attr("data-id");
   
-    // Now make an ajax call for the Article
+    ///Article ajax call
     $.ajax({
       method: "GET",
       url: "/articles/" + thisId
     })
-      // With that done, add the note information to the page
+      /// append note information 
       .then(data => {
         console.log(data);
-        // The title of the article
+        // Title
         $("#notes").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
+        // Enter title
         $("#notes").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
+        // Textarea
         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
+        // Submit Button
         $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
   
-        // If there's a note in the article
+        //insert note title & body in appended areas
         if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
+          $("#titleinput").val(data.note.title)
           $("#bodyinput").val(data.note.body);
         }
       });
   });
   
-  // When you click the savenote button
+  // On Click function to save note
   $(document).on("click", "#savenote", function() {
-    // Grab the id associated with the article from the submit button
     const thisId = $(this).attr("data-id");
   
-    // Run a POST request to change the note, using what's entered in the inputs
+  //ajax call to post inputs 
     $.ajax({
       method: "POST",
       url: "/articles/" + thisId,
       data: {
-        // Value taken from title input
         title: $("#titleinput").val(),
-        // Value taken from note textarea
         body: $("#bodyinput").val()
       }
     })
-      // With that done
       .then(data => {
-        // Log the response
         console.log(data);
-        // Empty the notes section
         $("#notes").empty();
       });
   
-    // Also, remove the values entered in the input and textarea for note entry
+    // Empty input values 
     $("#titleinput").val("");
     $("#bodyinput").val("");
   });
 
+  //on click function to caputre data and post to #saved route
     $(document).on("click", "#save", function() {
       window.location.reload();
       let thisId = $(this).attr("data-id");
 
-    //ajax call
+    //ajax call to post to #saved data
     $.ajax({
       method: "POST",
       url: "/saved" + thisId
@@ -98,19 +90,20 @@ $.getJSON("/articles", data => {
         url: "/articles/" + thisId,
       })
         .then(function(data){
-
         });
     });
 
-    $.getJSON("/saved", function (data) {
+  //grab information and display in json
+    $.getJSON("/saved", data => {
       for (let i = 0; i < data.length; i++){
         $("#saved").append("<p data-id='" + data[i]._id + "'>" +
       data[i].title + "<br />" +
       "<a href=" + data[i].link + "> " + data[i].link + "</a> " + "<br />" +
       "<button class='btn' id='noteBtn' data-id=" + data[i]._id + ">Article Notes</button>" +
       "<button class='btn' id='delete'  data-id=" + data[i]._id + ">Delete Article</button>" + "</p>");
-  }
-  console.log(data)
+      } 
+ 
+      console.log(data)
     });
 
   
