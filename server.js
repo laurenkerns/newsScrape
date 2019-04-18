6429b59c05778cef8b45c395506e53b754edb5a0
laurenkerns <laurenkerns@gmail.com>
2019-04-18 12:46:55 -0400

    finialized routes in server.js

--- server.js
+++ server.js
@@ -27,137 +27,93 @@ app.use(express.static("public"));
 
 
 //////////Connect to Mongo Database///////////////
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true })
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true })
 // const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 // mongoose.connect(MONGODB_URI);
 
-/////////ROUTES//////////////////
+///////////////ROUTES//////////////////
 
-app.get("/scrape", function(req, res) {
-  axios.get("https://www.nytimes.com/section/arts/").then(function(response) {
-    var $ = cheerio.load(response.data);
 
-    $("article h2").each(function(i, element) {
-      var result = {};
-        result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
+////////Route to scrape NY TImes website /////////
+app.get("/scrape", (req, res) => {
+  axios.get("https://www.nytimes.com/section/arts/").then(response => {
+    //load into cherro
+    const $ = cheerio.load(response.data);
+    //grab all H2's and links
+    $("article h2").each((i, element) => {
+      const result = {};
+        result.title = $(element).children("a").text();
+        result.link = $(element).children("a").attr("href");
+      //create new article 
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(dbArticle => {
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(err => {
           console.log(err);
         });
     });
-
-    res.send("Scrape Complete");
+    //redirect to homepage
+    res.redirect('/')
   });
 });
 
-app.get("/articles", function(req, res) {
+
+
+///////route to get all articles//////////
+app.get("/articles", (req, res) => {
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(dbArticle => {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(err => {
       res.json(err);
     });
 });
 
-app.get("/articles/:id", function(req, res) {
+
+
+/////route for saved articles///////
+app.get("/saved", (req, res) => {
+  db.Article.find({ "saved": true })   
+    .then(dbArticle => {
+      res.json(dbArticle);
+    })
+      .catch(err => {
+        res.json(err);
+      });
+});
+
+
+
+
+//////////route to get specific artice by id & add note///////////
+app.get("/articles/:id", (req, res) => {
   db.Article.findOne({ _id: req.params.id })
+  /////populate article with it's note
     .populate("note")
-    .then(function(dbArticle) {
+    .then(dbArticle => {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(err => {
       res.json(err);
     });
 });
 
-app.post("/articles/:id", function(req, res) {
+
+////////route to save new note to article/////////
+app.post("/articles/:id", (req, res) => {
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(dbNote => {
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(dbArticle => {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(err => {
       res.json(err);
     });
 });
-
-// /////////////////GET Route for scraping Reddit///////////
-// app.get("/scrape", (req, res) =>{
-//   //grab html with axios
-//   axios.get("http://www.echojs.com/").then(response => {
-//     const $ = cheerio.load(response.data);
-//     $("article h2").each((i, element) => {
-//       const result = {};
-//       result.title = $(element).children("a").text();
-//       result.link = $(element).children().attr("href");
-
-//   //create new article
-//       db.Article.create(result)
-//       .then(dbArticle => {
-//         console.log(dbArticle);
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//     });
-//     res.send("Articles Scraped")
-//   });
-// });
-
-
-
-// //////////////GET Route to pull articles from dbArticle/////////////
-// app.get("/articles", (req, res) => {
-//   db.Article.find({})
-//     .then(dbArticle => {
-//       res.json(dbArticle);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-
-// /////////////Get Request to get specific Article/////////////////////////
-// app.get("/articles/:id", (req, res) => {
-//   db.Article.findOne({ _id: req.params.id })
-//     .populate("note")
-//     .then(dbArticle => {
-//       res.json(dbArticle);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-
-
-// ///////////////Route to get note/////////////////
-// app.post("/articles/:id", (req, res) => {
-//   db.Note.create(req.body)
-//     .then(dbNote => {
-//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-//     })
-//     .then(dbArticle => {
-//       res.json(dbArticle);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
 
 
 
