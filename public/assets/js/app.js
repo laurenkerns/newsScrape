1883f3186ca952ae25980e8a6afe24a3d57e8d1e
laurenkerns <laurenkerns@gmail.com>
2019-04-18 13:05:54 -0400

    some app.js work, buttons still not working

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,109 +1,125 @@
-//////////Grab the articles as a json///////////
+////// Grab the articles as a json//////
 $.getJSON("/articles", data => {
-    // For each one
-    for (let i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "<a href=" + data[i].link + "> " + data[i].link + "</a> " + "<br />" +
-          "<button class='btn' id='note' data-id=" + data[i]._id + ">Article Notes</button>" +
-          "<button class='btn' id='save'  data-id=" + data[i]._id + ">Save Article</button>" +
-          "<button class='btn' id='delete'  data-id=" + data[i]._id + ">Delete Article</button>" + "</p>");
-    }
-  });
-  
-  ///////// On CLick note function///////////////
-  $(document).on("click", "#note", function() {
-    // Empty the notes 
-    $("#notes").empty();
-    // Save the id from the note id
-    const thisId = $(this).attr("data-id");
-  
-    ///Article ajax call
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      /// append note information 
-      .then(data => {
-        console.log(data);
-        // Title
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        // Enter title
-        $("#notes").append("<input id='titleinput' name='title' >");
-        // Textarea
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // Submit Button
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-  
-        //insert note title & body in appended areas
-        if (data.note) {
-          $("#titleinput").val(data.note.title)
-          $("#bodyinput").val(data.note.body);
-        }
-      });
-  });
-  
-  // On Click function to save note
-  $(document).on("click", "#savenote", function() {
-    const thisId = $(this).attr("data-id");
-  
-  //ajax call to post inputs 
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        title: $("#titleinput").val(),
-        body: $("#bodyinput").val()
+  for (let i = 0; i < data.length; i++) {
+   
+    ////// Display information on the page///////
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "<a href=" + data[i].link + "> " + data[i].link + "</a> " + "<br />" +
+      "<button class='waves-effect waves-light btn' id='noteBtn' data-id=" + data[i]._id + ">Article Notes</button>" +
+      "<button class='waves-effect waves-light btn' id='save'  data-id=" + data[i]._id + ">Save Article</button>" +
+      "<button class='waves-effect waves-light btn' id='delete'  data-id=" + data[i]._id + ">Delete Article</button>" + "</p>");
+  }
+});
+
+/////ON CLICK FUNCTION for note button
+$(document).on("click", "#noteBtn", function() {
+  $("#notes").empty();
+  // Save the id from the p tag
+  const thisId = $(this).attr("data-id");
+
+  /////// ajax call for article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+
+    /////// Add the note information to the page
+    .then(function(data) {
+      console.log(data);
+      /////TITLE
+      $("#notes").append("<h2>" + data.title + "</h2>");
+
+      ///// An INPUT TO ENTER TITLE
+      $("#notes").append("<input id='titleinput' name='title' >");
+
+      ///// TEXT AREA
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+
+      // SUBMIT BUTTON
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>"); ////add class
+
+      //////IF NO NOTE
+      if (data.note) {
+        //PUT TITLE OF NOT IN PNPUT
+        $("#titleinput").val(data.note.title);
+        // PUT BODY OF NOTE IN TEXT
+        $("#bodyinput").val(data.note.body);
       }
-    })
-      .then(data => {
-        console.log(data);
-        $("#notes").empty();
-      });
+    });
+});
+
+
+/////ON CLICK SAVE NOTE  
+  $(document).on("click", "#savenote", function() {
+  // GRAB ID
+  const thisId = $(this).attr("data-id");
+
+  // POST REQUEST TO CHANGE NOTE
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val()
+    }
+  })
+    .then(function(data) {
+      // EMPTY NOTES
+      $("#notes").empty();
+    });
+
+  // REMOVE INPUT AREA
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+
+
+///////ON CLICK FUNCTION TO SAVE
+$(document).on("click", "#save", function () {
+  /////GRAB ID
+  const thisId = $(this).attr("data-id");
+
+  ////POST REQUEST TO UPDATE NOTE
+  $.ajax({
+    method: "POST",
+    url: "/saved/" + thisId,
+  })
+    .then(function (data) {
+      // console.log(data);
+    });
+});
+
+
+
+//////ON CLICK TO DELETE NOTE
+$(document).on("click", "#delete", function () {
+  const thisId = $(this).attr("data-id");
   
-    // Empty input values 
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
+  //////reload the page 
+  window.location.reload();
 
-  //on click function to caputre data and post to #saved route
-    $(document).on("click", "#save", function() {
-      window.location.reload();
-      let thisId = $(this).attr("data-id");
-
-    //ajax call to post to #saved data
-    $.ajax({
-      method: "POST",
-      url: "/saved" + thisId
-    })
-      .then(function(data) {
-        console.log(data);
-      });
+  ///////ajax delete request
+  $.ajax({
+    method: "DELETE",
+    url: "/articles/" + thisId, 
+  }) 
+    .then(function (data) {   
     });
+});
 
-    $(document).on("click", "#delete", function (){
-      let thisId = $(this).attr("data-id");
-      console.log("delete sucessful");
-      window.location.reload();
 
-      $.ajax({
-        method: "DELETE",
-        url: "/articles/" + thisId,
-      })
-        .then(function(data){
-        });
-    });
 
-  //grab information and display in json
-    $.getJSON("/saved", data => {
-      for (let i = 0; i < data.length; i++){
-        $("#saved").append("<p data-id='" + data[i]._id + "'>" +
-      data[i].title + "<br />" +
-      "<a href=" + data[i].link + "> " + data[i].link + "</a> " + "<br />" +
-      "<button class='btn' id='noteBtn' data-id=" + data[i]._id + ">Article Notes</button>" +
-      "<button class='btn' id='delete'  data-id=" + data[i]._id + ">Delete Article</button>" + "</p>");
-      } 
- 
-      console.log(data)
-    });
 
+////////GET SAVED ARTICLES
+$.getJSON("/saved", function (data) {
+  
+  // For each one
+  for (let i = 0; i < data.length; i++) {
+
+    // DISPLAY SAVED ARTICLES AT BOTTOM
+    $("#saved").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "<a href=" + data[i].link + "> " + data[i].link + "</a> " + "<br />" +
+        "<button class='waves-effect waves-light btn' id='noteBtn' data-id=" + data[i]._id + ">Article Notes</button>" +
+        "<button class='waves-effect waves-light btn' id='delete'  data-id=" + data[i]._id + ">Delete Article</button>" + "</p>");
+  }
+});
   
